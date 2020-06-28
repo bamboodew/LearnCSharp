30a0abf61261d45f82ad9dd2faa79955b4ec408e
bamboodew <mrbamboodew@gmail.com>
2020-06-28 20:54:17 +0800

    TrafficLights

--- TrafficLights/TrafficLights/Form1.Designer.cs
+++ TrafficLights/TrafficLights/Form1.Designer.cs
@@ -30,20 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape4 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape5 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape6 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.lineShape7 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.lineShape8 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape7 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape6 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape5 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape4 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.button_Start = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button_Stop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // shapeContainer1
@@ -64,53 +65,13 @@
             this.shapeContainer1.TabIndex = 0;
             this.shapeContainer1.TabStop = false;
             // 
-            // lineShape1
+            // lineShape8
             // 
-            this.lineShape1.Name = "lineShape1";
-            this.lineShape1.X1 = 270;
-            this.lineShape1.X2 = 270;
-            this.lineShape1.Y1 = 28;
-            this.lineShape1.Y2 = 138;
-            // 
-            // lineShape2
-            // 
-            this.lineShape2.Name = "lineShape2";
-            this.lineShape2.X1 = 422;
-            this.lineShape2.X2 = 422;
-            this.lineShape2.Y1 = 25;
-            this.lineShape2.Y2 = 135;
-            // 
-            // lineShape3
-            // 
-            this.lineShape3.Name = "lineShape3";
-            this.lineShape3.X1 = 269;
-            this.lineShape3.X2 = 101;
-            this.lineShape3.Y1 = 138;
-            this.lineShape3.Y2 = 138;
-            // 
-            // lineShape4
-            // 
-            this.lineShape4.Name = "lineShape4";
-            this.lineShape4.X1 = 266;
-            this.lineShape4.X2 = 98;
-            this.lineShape4.Y1 = 277;
-            this.lineShape4.Y2 = 277;
-            // 
-            // lineShape5
-            // 
-            this.lineShape5.Name = "lineShape5";
-            this.lineShape5.X1 = 590;
-            this.lineShape5.X2 = 422;
-            this.lineShape5.Y1 = 135;
-            this.lineShape5.Y2 = 135;
-            // 
-            // lineShape6
-            // 
-            this.lineShape6.Name = "lineShape6";
-            this.lineShape6.X1 = 594;
-            this.lineShape6.X2 = 426;
-            this.lineShape6.Y1 = 277;
-            this.lineShape6.Y2 = 277;
+            this.lineShape8.Name = "lineShape8";
+            this.lineShape8.X1 = 425;
+            this.lineShape8.X2 = 425;
+            this.lineShape8.Y1 = 277;
+            this.lineShape8.Y2 = 387;
             // 
             // lineShape7
             // 
@@ -120,80 +81,146 @@
             this.lineShape7.Y1 = 276;
             this.lineShape7.Y2 = 386;
             // 
-            // lineShape8
+            // lineShape6
             // 
-            this.lineShape8.Name = "lineShape8";
-            this.lineShape8.X1 = 425;
-            this.lineShape8.X2 = 425;
-            this.lineShape8.Y1 = 277;
-            this.lineShape8.Y2 = 387;
+            this.lineShape6.Name = "lineShape6";
+            this.lineShape6.X1 = 594;
+            this.lineShape6.X2 = 426;
+            this.lineShape6.Y1 = 277;
+            this.lineShape6.Y2 = 277;
+            // 
+            // lineShape5
+            // 
+            this.lineShape5.Name = "lineShape5";
+            this.lineShape5.X1 = 590;
+            this.lineShape5.X2 = 422;
+            this.lineShape5.Y1 = 135;
+            this.lineShape5.Y2 = 135;
+            // 
+            // lineShape4
+            // 
+            this.lineShape4.Name = "lineShape4";
+            this.lineShape4.X1 = 266;
+            this.lineShape4.X2 = 98;
+            this.lineShape4.Y1 = 277;
+            this.lineShape4.Y2 = 277;
+            // 
+            // lineShape3
+            // 
+            this.lineShape3.Name = "lineShape3";
+            this.lineShape3.X1 = 269;
+            this.lineShape3.X2 = 101;
+            this.lineShape3.Y1 = 138;
+            this.lineShape3.Y2 = 138;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 422;
+            this.lineShape2.X2 = 422;
+            this.lineShape2.Y1 = 25;
+            this.lineShape2.Y2 = 135;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 270;
+            this.lineShape1.X2 = 270;
+            this.lineShape1.Y1 = 28;
+            this.lineShape1.Y2 = 138;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // button_Start
             // 
-            this.button_Start.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.button_Start.BackColor = System.Drawing.Color.LightGray;
+            this.button_Start.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.button_Start.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button_Start.Location = new System.Drawing.Point(652, 41);
             this.button_Start.Name = "button_Start";
             this.button_Start.Size = new System.Drawing.Size(105, 44);
             this.button_Start.TabIndex = 1;
-            this.button_Start.Text = "开始工作";
-            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Text = "开始";
+            this.button_Start.UseVisualStyleBackColor = false;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(352, 114);
+            this.label1.Location = new System.Drawing.Point(382, 106);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 22);
+            this.label1.Size = new System.Drawing.Size(30, 30);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "10";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LimeGreen;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.Location = new System.Drawing.Point(275, 277);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 22);
+            this.label2.Size = new System.Drawing.Size(30, 30);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "10";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(211, 151);
+            this.label3.Location = new System.Drawing.Point(237, 147);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 22);
+            this.label3.Size = new System.Drawing.Size(30, 30);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "10";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
             this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(422, 246);
+            this.label4.Location = new System.Drawing.Point(426, 239);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 22);
+            this.label4.Size = new System.Drawing.Size(30, 30);
             this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.label4.Text = "10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.BackColor = System.Drawing.Color.LightGray;
+            this.button_Stop.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button_Stop.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_Stop.Location = new System.Drawing.Point(652, 103);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(105, 44);
+            this.button_Stop.TabIndex = 6;
+            this.button_Stop.Text = "停止";
+            this.button_Stop.UseVisualStyleBackColor = false;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
             // 
             // Form_TrafficLights
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Stop);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_Start);
             this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form_TrafficLights";
             this.Text = "红绿灯";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -214,6 +241,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_Stop;
     }
 }
 
--- TrafficLights/TrafficLights/Form1.cs
+++ TrafficLights/TrafficLights/Form1.cs
@@ -1,20 +1,126 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
+
+/*
+ * 红绿灯系统：
+ * 停——红灯10秒
+ * 行——绿灯7秒，黄灯3秒
+ * 
+ * 按键1：开始、暂停、恢复
+ * 按键2：停止
+ */
 
 namespace TrafficLights
 {
     public partial class Form_TrafficLights : Form
     {
+        private int count = 0;                      // 计数器
+        private int time = 20;                  // 定义初始时间10秒
+
         public Form_TrafficLights()
         {
             InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, System.EventArgs e)
+        {
+            count++;                            // 计数器自加一
+            if (time - count == 0)
+            {
+                count = 0;
+                label1.BackColor = Color.LimeGreen;
+                label2.BackColor = Color.LimeGreen;
+                label3.BackColor = Color.Red;
+                label4.BackColor = Color.Red;
+            }
+            if (time - count > 10)
+            {
+                label1.Text = (time - 10 - count).ToString();
+                label2.Text = (time - 10 - count).ToString();
+
+                label3.Text = (time - 10 - count).ToString();
+                label4.Text = (time - 10 - count).ToString();
+
+                if (time - count > 13)
+                {
+                    label1.BackColor = Color.LimeGreen;
+                    label2.BackColor = Color.LimeGreen;
+                    label3.BackColor = Color.Red;
+                    label4.BackColor = Color.Red;
+                }
+                else
+                {
+                    label1.BackColor = Color.Yellow;
+                    label2.BackColor = Color.Yellow;
+                    label3.BackColor = Color.Red;
+                    label4.BackColor = Color.Red;
+                }
+            }
+            else if (time - count < 11 && time - count > 0)
+            {
+                label1.Text = (time - count).ToString();
+                label2.Text = (time - count).ToString();
+
+                label3.Text = (time - count).ToString();
+                label4.Text = (time - count).ToString();
+
+                label1.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+
+                if (time - count > 3)
+                {
+                    label3.BackColor = Color.LimeGreen;
+                    label4.BackColor = Color.LimeGreen;
+                }
+                else
+                {
+                    label3.BackColor = Color.Yellow;
+                    label4.BackColor = Color.Yellow;
+                }
+            }
+
+
+        }
+
+        private void button_Start_Click(object sender, System.EventArgs e)
+        {
+            if (button_Start.Text == "开始" || button_Start.Text == "恢复")
+            {
+                button_Start.Text = "暂停";
+                button_Start.BackColor = Color.LimeGreen;
+
+                // 计时器开始工作
+                timer.Start();
+            }
+            else
+            {
+                button_Start.Text = "恢复";
+                button_Start.BackColor = Color.LightGray;
+
+                // 计时器暂停工作
+                timer.Stop();
+            }
+        }
+
+        private void button_Stop_Click(object sender, System.EventArgs e)
+        {
+            // 全部清零复位
+            timer.Stop();
+            time = 20;
+            count = 0;
+
+            button_Start.Text = "开始";
+            button_Start.BackColor = Color.LightGray;
+
+            label1.Text = 10.ToString();
+            label2.Text = 10.ToString();
+            label3.Text = 10.ToString();
+            label4.Text = 10.ToString();
+
+            label1.BackColor = Color.LimeGreen;
+            label2.BackColor = Color.LimeGreen;
+            label3.BackColor = Color.Red;
+            label4.BackColor = Color.Red;
         }
     }
 }
